ca032c6e2084e189ae1aab8c80ea6439821a711d
William Suryawan <suryawan.william@gmail.com>
2019-01-13 17:42:21 +0700

    tambah file

---
+++ William-P0-W2-E4.js
@@ -0,0 +1,22 @@
+var tanggal = 23; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 12; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 2018; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+if (tanggal<1 || tanggal>31 || tahun<1900 || tahun>2200 || bulan<1 || bulan>12) {
+console.log ("Masukkan angka yang benar");
+} else {
+  switch (bulan) {
+    case 1: { console.log (tanggal + " Januari " + tahun); break;}
+    case 2: { console.log (tanggal + " Februari " + tahun); break;}
+    case 3: { console.log (tanggal + " Maret " + tahun); break;}
+    case 4: { console.log (tanggal + " April "+tahun); break;}
+    case 5: { console.log (tanggal + " Mei "+tahun); break;}
+    case 6: { console.log (tanggal + " Juni "+tahun); break;}
+    case 7: { console.log (tanggal + " Juli "+tahun); break;}
+    case 8: { console.log (tanggal + " Agustus "+tahun); break;}
+    case 9: { console.log (tanggal + " September "+tahun); break;}
+    case 10: { console.log (tanggal + " Oktober "+tahun); break;}
+    case 11: { console.log (tanggal + " November "+tahun); break;}
+    case 12: { console.log (tanggal + " Desember "+tahun); break;}
+  }
+}
